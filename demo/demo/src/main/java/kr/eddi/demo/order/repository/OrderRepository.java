d72c7f87d56ce7f6a5df27a30fb31bd719b61d90
iknowca <iknowca@gmail.com>
2023-05-28 20:36:07 +0900

    feat: [김원기] userToken기반 주문리스트 조회기능 구현 [SPP-ORDER-LIST-1]

--- demo/demo/src/main/java/kr/eddi/demo/order/repository/OrderRepository.java
+++ demo/demo/src/main/java/kr/eddi/demo/order/repository/OrderRepository.java
@@ -2,6 +2,11 @@ package kr.eddi.demo.order.repository;
 
 import kr.eddi.demo.order.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    @Query("select o from product_order o join fetch o.product where o.account.userToken=:userToken")
+    List<Order> findByUserToken(String userToken);
 }
