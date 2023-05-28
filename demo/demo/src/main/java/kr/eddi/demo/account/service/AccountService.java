8ff8460ef7c9908dbdbfcb30e1199fc90dad0d9a
qnfthslr <105690509+qnfthslr@users.noreply.github.com>
2023-05-29 01:02:58 +0900

    feat: [이상원] 회원들의 이메일 및 비밀번호 확인 기능 추가 [SPP-ACCOUNTLIST-1]

--- demo/demo/src/main/java/kr/eddi/demo/account/service/AccountService.java
+++ demo/demo/src/main/java/kr/eddi/demo/account/service/AccountService.java
@@ -4,10 +4,14 @@ import kr.eddi.demo.account.controller.form.AccountRegistRequestForm;
 import kr.eddi.demo.account.controller.form.AccoutLoginRequestForm;
 import kr.eddi.demo.account.entity.Account;
 
+import java.util.List;
+
 public interface AccountService {
     Account regist(AccountRegistRequestForm requestForm);
 
     Account login(AccoutLoginRequestForm requestForm);
 
     Boolean checkEmail(String email);
+
+    List<Account> list();
 }
