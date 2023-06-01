acf22c80446a457f8cdd272e0de441a8ceea9b68
wonki kim <iknowca@gmail.com>
2023-06-02 01:25:52 +0900

    update: [김원기] 사진 등록 기능을 삭제합니다. [ SPP-PRODUCT-REGIST-4]

--- demo/demo/src/main/java/kr/eddi/demo/product/service/ProductServiceImpl.java
+++ demo/demo/src/main/java/kr/eddi/demo/product/service/ProductServiceImpl.java
@@ -3,7 +3,6 @@ package kr.eddi.demo.product.service;
 import kr.eddi.demo.account.entity.Account;
 import kr.eddi.demo.account.repository.AccountRepository;
 import kr.eddi.demo.product.entity.Product;
-import kr.eddi.demo.product.entity.ProductImagePath;
 import kr.eddi.demo.product.repository.ProductRepository;
 import kr.eddi.demo.product.service.form.ProductResponseForm;
 import kr.eddi.demo.product.service.form.RegistRequestform;
@@ -32,48 +31,20 @@ public class ProductServiceImpl implements ProductService {
 
 
     @Override
-    public Product regist(List<MultipartFile> images,
-                          RegistRequestform requestForm) {
+    public Product regist(RegistRequestform requestForm) {
         log.info("/product/regist()");
         String userToken = requestForm.getUserToken();
         Optional<Account> maybeAccount = accountRepository.findByUserToken(userToken);
         if (maybeAccount.isEmpty()) {
             return null;
         }
-        log.info("\u001B[32m"+"Account: green"+"\u001B[0m");
+        log.info("account: " + maybeAccount.get());
         Product product = requestForm.toProduct(maybeAccount.get());
-        log.info("Product: green");
-        if (!writeImages(images, product)) {
-            return null;
-        }
 
-        log.info("\u001B[32m"+"writeImage(): green"+"\u001B[0m");
         return productRepository.save(product);
     }
 
-    public boolean writeImages(List<MultipartFile> images, Product product)  {
-        log.info("writeImage()");
 
-        for (MultipartFile image : images) {
-            String originalFileName = image.getOriginalFilename();
-            try {
-                FileOutputStream writer = new FileOutputStream(
-                        "./downloadedImageFiles/" + originalFileName
-                );
-
-                writer.write(image.getBytes());
-
-                writer.close();
-                product.addImage(new ProductImagePath(originalFileName));
-
-            } catch (IOException e) {
-                e.printStackTrace();
-                return false;
-            }
-
-        }
-        return true;
-    }
 
     @Override
     public ProductResponseForm requestProduct(Long productId) {
